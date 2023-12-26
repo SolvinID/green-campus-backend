030cabb5dc438106134ab268743e5281e7e03170
wicahma <teguh.dc.kusuma@gmail.com>
2023-12-26 14:25:02 +0700

    update: updating mysql configuration

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,7 @@ module.exports = {
         password: config.dbPass,
         database: config.dbName,
         host: config.dbHost,
-        dialect: 'mysql',
+        dialect: 'mysql2',
         dialectOptions: {
             bigNumberStrings: true,
         },
@@ -16,7 +16,7 @@ module.exports = {
         password: config.dbPass,
         database: config.dbName,
         host: config.dbHost,
-        dialect: 'mysql',
+        dialect: 'mysql2',
         dialectOptions: {
             bigNumberStrings: true,
         },
@@ -27,7 +27,7 @@ module.exports = {
         database: config.dbName,
         host: config.dbHost,
         port: config.port,
-        dialect: 'mysql',
+        dialect: 'mysql2',
         dialectOptions: {
             bigNumberStrings: true,
         },
